fe50b27feeb0faf19416d9834d8a4571cecc2db4
HarryPaniccc <harrysq2@gmail.com>
2024-05-14 20:19:40 +0200

    Drake is a pedophile

--- digit_recognition_tutorial.py
+++ digit_recognition_tutorial.py
@@ -7,4 +7,15 @@ import tensorflow as tf
 
 mnist = tf.keras.datasets.mnist #Get dataset, all labeled
 
+#Sets train data and labels, as well as test data and labels
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+
+#Normalizing all shit to be not 0,255 but 0,1
+x_train = tf.keras.utils.normalize(x_train, axis = 1)
+x_test = tf.keras.utils.normalize(x_test, axis = 1)
+
+model = tf.keras.models.Sequential() #Basic?
+
+model.add(tf.keras.layers.Flatten(input_shape = (28,28)))
 
