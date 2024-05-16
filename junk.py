c74c3588e0d5332f4486b38eb5fcd3d8c705e270
Harry Papanicolaou <harrysq2@gmail.com>
2024-05-16 22:47:22 +0200

    get_dataset fixed

--- junk.py
+++ junk.py
@@ -1,23 +1,34 @@
-import os
+# import os
 
 
-directory = f"shape_recognition/shapesdataset/test_set/angleCross"
-number_files = len(os.listdir(directory))
+# directory = f"shape_recognition/shapesdataset/test_set/angleCross"
+# number_files = len(os.listdir(directory))
 
-print(number_files)
-
-
-address = f"shape_recognition/shapesdataset/test_set"
-shape = f"angleCross"
-training_set_size = len(os.listdir(f"{address}/{shape}"))
-print(training_set_size)
+# print(number_files)
 
 
-count = 1
-print(f"{address}/{shape}/{shape}_{"{:02d}".format(count)}.jpg")
+# address = f"shape_recognition/shapesdataset/test_set"
+# shape = f"angleCross"
+# training_set_size = len(os.listdir(f"{address}/{shape}"))
+# print(training_set_size)
 
-while os.path.isfile(f"{address}/{shape}/{shape}_{"{:02d}".format(count)}.jpg"): # so long as there is a shape here
-    count += 1
+
+# count = 1
+# print(f"{address}/{shape}/{shape}_{"{:02d}".format(count)}.jpg")
+
+# while os.path.isfile(f"{address}/{shape}/{shape}_{"{:02d}".format(count)}.jpg"): # so long as there is a shape here
+#     count += 1
 
 
-print(count)
+# print(count)
+
+
+shapes = [f"angleCross", 
+          f"ellipse", 
+          f"hexagon", 
+          f"line", 
+          f"square", 
+          f"straightCross", 
+          f"triangle"]
+
+print(len(shapes))
--- shape_recognition/shape_recognition_training.py
+++ shape_recognition/shape_recognition_training.py
@@ -7,21 +7,28 @@ import tensorflow as tf
 # Step 1: Import all the images as a datase
 
 #Reads in x_train and y_train values from a folder
-def get_data_set(address, shape): # returns x and y of a target address 
+def get_dataset(address, shape): # returns x and y of a target address 
 
     training_set_size = len(os.listdir(f"{address}/{shape}"))
+
+    if address == f"shape_recognition/shapesdataset/training_set":
+        shape_number = 41
+        index_difference = 41
+    else:
+        shape_number = 1
+        index_difference = 1
+	
 
     x = np.empty((training_set_size, 64, 64))
     y = np.full(training_set_size,shape, dtype='<U20')
     
-    shape_number = 1
 
     while os.path.isfile(f"{address}/{shape}/{shape}_{"{:02d}".format(shape_number)}.jpg"): # so long as there is a shape here, excuse gross address formatting
         try:
             img = cv2.imread(f"{address}/{shape}/{shape}_{"{:02d}".format(shape_number)}.jpg")[:,:,0]
             img = np.invert(np.array([img]))
-            x[shape_number - 1] = img #again, indexing is broken because of how dataset is labelled
-            y[shape_number - 1] = shape
+            x[shape_number - index_difference] = img #again, indexing is broken because of how dataset is labelled
+            y[shape_number - index_difference] = shape
             #x_train[0] = img
         except:
             #Failed
@@ -31,21 +38,29 @@ def get_data_set(address, shape): # returns x and y of a target address
             #idfk how this works
     return (x, y)
 
-# Defining our target addresses
-addresses = [f"shape_recognition/shapesdataset/training_set", f"shape_recognition/shapesdataset/test_set"]
-shapes = [f"angleCross", f"ellipse", f"hexagon", f"line", f"square", f"straightCross", f"triangle"]
+# Defining our target addresses and labels
+addresses = [f"shape_recognition/shapesdataset/training_set",
+             f"shape_recognition/shapesdataset/test_set"]
+
+shapes = [f"angleCross", 
+          f"ellipse", 
+          f"hexagon", 
+          f"line", 
+          f"square", 
+          f"straightCross", 
+          f"triangle"]
 
 for i in range(len(shapes)):
-    (x_train_new, y_train_new) = get_data_set(addresses[0], shapes[i])
-    print(x_train_new[1])
+    (x_train_new, y_train_new) = get_dataset(addresses[0], shapes[i])
+  
+    plt.imshow(x_train_new[1], cmap = plt.cm.binary)
+    plt.show()
+
     if i == 0:
         x_train = x_train_new
         y_train = y_train_new
-        plt.imshow(x_train[1], cmap = plt.cm.binary)
-        plt.show()
-        print(y_train[1])
         continue
-
+    
     x_train = np.append(x_train, x_train_new)
     y_train = np.append(y_train, y_train_new)
 
@@ -53,9 +68,9 @@ print(y_train[100])
 
 address1 = f"shape_recognition/shapesdataset/test_set"
 shape1 = f"angleCross"
-(x_train, y_train) = get_data_set(address1, shape1)
+(x_train, y_train) = get_dataset(address1, shape1)
 # # Proves we have got what we want
-plt.imshow(x_train[1], cmap = plt.cm.binary)
+plt.imshow(x_train[51], cmap = plt.cm.binary)
 plt.show()
-print(y_train[1])
-print(x_train[1])
+print(y_train[51])
+print(x_train[51])
