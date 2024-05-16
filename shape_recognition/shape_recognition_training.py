01fd5bcce38ab3e12cea05da32233eab02b005d8
HarryPaniccc <harrysq2@gmail.com>
2024-05-16 16:25:10 +0200

    Ugh

--- shape_recognition/shape_recognition_training.py
+++ shape_recognition/shape_recognition_training.py
@@ -7,16 +7,21 @@ import tensorflow as tf
 # Step 1: Import all the images as a datase
 
 #Reads in x_train and y_train values from a folder
-def train_from(address, shape): # Both must be fstrings, 
+def training_from(address, shape): # Both must be fstrings, 
+
     training_set_size = len(os.listdir(f"{address}/{shape}"))
-    y_train = np.array(training_set_size)
-    x_train = np.array(training_set_size)
+    x_train = [np.empty(64 * 64) for _ in range(training_set_size)]
+    y_train = [np.empty() for _ in range(training_set_size)]
     
     shape_number = 1
 
-    while os.path.isfile(f"{address}/{shape}_{"{:02d}".format(shape_number)}.png"): # so long as there is a shape here
+    while os.path.isfile(f"{address}/{shape}/{shape}_{"{:02d}".format(shape_number)}.jpg"): # so long as there is a shape here
         try:
-            x_train.append
+            img = cv2.imread(f"{address}/{shape}/{shape}_{"{:02d}".format(shape_number)}.jpg")
+            img = np.array([img])
+            x_train[shape_number - 1] = img
+            y_train[shape_number - 1] = shape
+            #x_train[0] = img
         except:
             #Failed
             print("Error!")
@@ -25,4 +30,10 @@ def train_from(address, shape): # Both must be fstrings,
             #idfk how this works
     return (x_train, y_train)
 
+address1 = f"shape_recognition/shapesdataset/test_set"
+shape1 = f"angleCross"
+(x_train, y_train) = training_from(address1, shape1)
 
+# Proves we have got what we want
+# plt.imshow(x_train[1][0], cmap = plt.cm.binary)
+# plt.show()
