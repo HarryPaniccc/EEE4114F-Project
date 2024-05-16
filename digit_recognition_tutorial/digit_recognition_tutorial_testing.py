d73b3c2a7db2235ac0c6b536b1d8eba9aad8185a
HarryPaniccc <harrysq2@gmail.com>
2024-05-16 12:24:07 +0200

    moving stuff aroudn and shit

--- digit_recognition_tutorial/digit_recognition_tutorial_testing.py
+++ digit_recognition_tutorial/digit_recognition_tutorial_testing.py
@@ -9,12 +9,12 @@ model = tf.keras.models.load_model('handwritten.keras')
 
 image_number = 1
 
-while os.path.isfile(f"digit_tests/digit{image_number}.png"):
+while os.path.isfile(f"digit_recognition_tutorial/digit_tests/digit{image_number}.png"):
     try:
-        img = cv2.imread(f"digit_tests/digit{image_number}.png")[:,:,0]
+        img = cv2.imread(f"digit_recognition_tutorial/digit_tests/digit{image_number}.png")[:,:,0]
         img = np.invert(np.array([img]))
         prediction = model.predict(img)
-        print(f"This image is probable a {np.argmax(prediction)}")
+        print(f"This image is probably a {np.argmax(prediction)}")
         plt.imshow(img[0], cmap = plt.cm.binary)
         plt.show()
     except:
