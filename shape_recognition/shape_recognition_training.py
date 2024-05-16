f4ea43d1742f8d0e9c2ebb4e5dcbce8cebf8bdea
HarryPaniccc <harrysq2@gmail.com>
2024-05-16 14:10:56 +0200

    No idea why this commits so huge

---
+++ shape_recognition/shape_recognition_training.py
@@ -0,0 +1,12 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+# Step 1: Import all the images as a datase
+
+def train_from(address, shape): # Both must be fstrings
+    while os.path.isfile(address + f"")
+
+while os.path.isfile(f"shape_recognition/shapedataset/training_set/")
