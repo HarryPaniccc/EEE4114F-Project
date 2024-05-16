f16b5a339a2d42429ea9f0ba0e127bafb67dfd69
HarryPaniccc <harrysq2@gmail.com>
2024-05-16 15:29:45 +0200

    Too much voodoo

--- junk.py
+++ junk.py
@@ -9,5 +9,15 @@ print(number_files)
 
 address = f"shape_recognition/shapesdataset/test_set"
 shape = f"angleCross"
-training_set_size = len(os.listdir(address + f"/" + shape))
+training_set_size = len(os.listdir(f"{address}/{shape}"))
 print(training_set_size)
+
+
+count = 1
+print(f"{address}/{shape}/{shape}_{"{:02d}".format(count)}.jpg")
+
+while os.path.isfile(f"{address}/{shape}/{shape}_{"{:02d}".format(count)}.jpg"): # so long as there is a shape here
+    count += 1
+
+
+print(count)
--- shape_recognition/shape_recognition_training.py
+++ shape_recognition/shape_recognition_training.py
@@ -7,16 +7,19 @@ import tensorflow as tf
 # Step 1: Import all the images as a datase
 
 #Reads in x_train and y_train values from a folder
-def train_from(address, shape): # Both must be fstrings
-    training_set_size = len(os.listdir(address + f"/" + shape))
-    y_train = np.array(1)
-    y_train[0] = shape
+def train_from(address, shape): # Both must be fstrings, 
+    training_set_size = len(os.listdir(f"{address}/{shape}"))
+    y_train = np.array(training_set_size)
+    x_train = np.array(training_set_size)
+    
     shape_number = 1
-    while os.path.isfile(address + f"/training_set" + shape):
+
+    while os.path.isfile(f"{address}/{shape}_{"{:02d}".format(shape_number)}.png"): # so long as there is a shape here
         try:
             x_train.append
         except:
             #Failed
+            print("Error!")
         finally:
             shape_number += 1
             #idfk how this works
